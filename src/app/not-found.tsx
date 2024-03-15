873bba5cefdff1498817f6ed74f1710ec1e9f12a
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-03-15 19:01:11 -0300

    styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function NotFound() {
   return (
     <main id='not-found' className='flex flex-col h-screen'>
-      <section id="home" className="flex items-center max-w-[50%] mr-auto ml-[10%] min-h-[100%]">
+      <section id="home" className="flex items-center max-w-[50%] mr-auto ml-[11%] min-h-[100%]">
       <article className="flex flex-col gap-5">
         <h2 className="text-5xl text-slate-100 font-semibold">Page under construction!</h2>
         <p className="text-lg text-slate-400">We're currently working on building this page. <br /> Please come back later!</p>
