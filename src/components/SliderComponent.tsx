dc723d3b5dc978b75061fa1d7934c48f5a5de8e1
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-03-15 10:51:25 -0300

    Avances

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -15,7 +15,7 @@ const SliderComponent = () => {
       toRight={false}
       pauseOnHover={false}
       blurBorders={true}
-      borderBlurColor={"#b2b2b2"}
+      blurBoderColor={'#ffffff'}
     >
       <Slider.Slide style={{ width: "300px", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <svg width="151" height="36" viewBox="151 36" fill="none" xmlns="http://www.w3.org/2000/svg">
