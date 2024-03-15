6fe80c3f32cac836f3083076adb8e7507313d60d
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-03-15 20:37:43 -0300

    styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 
 export default function Home() {
   return (
-    <section id="home" className="flex items-center max-w-[50%] mr-auto ml-[11%] FHD:min-h-[80%] HD-1:min-h-[72%] pb-[10%]">
+    <section id="home" className="flex items-center max-w-[50%] mr-auto ml-[11%] FHD:min-h-[80%] HD-1:min-h-[72%] pb-[10%] bg-[#283345]">
       <article className="flex flex-col gap-5">
         <h2 className="text-5xl text-slate-100 font-semibold">We build beautiful and<br />reliable web solutions</h2>
         <p className="text-lg text-slate-400">We strongly believe that communication, trust and transparency <br /> are the essential elements of successful bussiness relationship. <br /> Ready for the journey?</p>
