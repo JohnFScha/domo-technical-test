79a68c15cc016338eefbad20be70be8583845bea
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-03-16 15:29:29 -0300

    styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,14 @@ import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <main id='not-found' className='flex flex-col h-screen'>
-      <section id="home" className="flex items-center max-w-[50%] mr-auto ml-[11%] min-h-[100%]">
-      <article className="flex flex-col gap-5">
+    <main id='not-found' className='flex min-h-screen'>
+      <section className="flex flex-col gap-5  min-h-[100%] bg-[#283345] my-auto ml-[11%]">
         <h2 className="text-5xl text-slate-100 font-semibold">Page under construction!</h2>
         <p className="text-lg text-slate-400">We're currently working on building this page. <br /> Please come back later!</p>
         <div className="flex gap-5 mt-3">
           <Link href={'/'} className="btn btn-outline rounded-none">Go back</Link>
         </div>
-      </article>
-    </section>
+      </section>
     </main>
   )
 }
